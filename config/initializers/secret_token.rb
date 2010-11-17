8daca90ae5223145c355d635762be01a1c8e201f
Franz Torghele <ftorghele.mmt-b2009@fh-salzburg.ac.at>
2010-11-17 22:01:58 +0100

    some changes for Heroku to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snups::Application.config.secret_token = '7d5db3d05bc6aeee88399f11f5069a940bcf5db30b9f4a835a521604bd92ddf3ceba12057cdd73198ab968a255de25b500758980b42dd8d8633111009b5ce4cc'
