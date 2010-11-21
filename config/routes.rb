c9f2fcac69a20e1dc7e5de298ddaf42625d5de36
Franz Torghele <ftorghele.mmt-b2009@fh-salzburg.ac.at>
2010-11-21 17:41:56 +0100

    login backend for android

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Snups::Application.routes.draw do
   get "home/settings"
   
   devise_for :users, :path_names => { :sign_up => "register"}
+  
+  resources :mobileLogin
 
   root :to => "home#index"
 end
---
+++ test/unit/helpers/mobile_login_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MobileLoginHelperTest < ActionView::TestCase
+end
