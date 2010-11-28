89340ad16c157076fd73e0d4a5dec333d7b7fa2a
Wolfgang Vogl <wuulf01@yahoo.de>
2010-11-28 17:15:48 +0100

    gravatastic included

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   
   include MiniMagick
   
+  include Gravtastic
+  gravtastic
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
