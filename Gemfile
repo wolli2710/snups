aa5a8fd5cc9c35b244f0c2ff0c3a8f0eca0f23e4
Franz Torghele <ftorghele.mmt-b2009@fh-salzburg.ac.at>
2010-12-04 14:49:56 +0100

    capistrano

--- Gemfile
+++ Gemfile
@@ -1,34 +1,15 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.1'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'devise', '1.1.3'
 gem 'builder', '~>2.0'
-
 gem 'mini_magick'
+gem 'capistrano'
 
-# Use unicorn as the web server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'ruby-debug'
-
-# Bundle the extra gems:
-# gem 'bj'
-# gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
-
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
 group :development, :test do
   gem 'sqlite3-ruby', :require => 'sqlite3'
-  #   gem 'webrat'
+end
+
+group :production do
+  gem 'mysql2'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "snups"
+set :repository,  "ssh://fhs31478@mediacube.at/var/www/virthosts/franzonrails.multimediatechnology.at/gitrepo"
+
+set :deploy_to, "/var/www/virthosts/franzonrails.multimediatechnology.at/"
+set :user, "fhs31478"
+set :use_sudo, false
+
+set :scm, :git
+set :branch, "master"
+default_run_options[:pty] = true
+
+role :web, "multimediatechnology.at"
+role :app, "multimediatechnology.at"
+role :db,  "multimediatechnology.at", :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :copy_config do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+require "bundler/capistrano"
+
+after "deploy:update_code", "deploy:copy_config"
