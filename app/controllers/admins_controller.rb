b91e871dfca7052c13e3cc0bc5201667ee165510
Franz Torghele <ftorghele.mmt-b2009@fh-salzburg.ac.at>
2010-12-30 22:09:39 +0100

    admin can add new admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,28 @@ class AdminsController < ApplicationController
   before_filter :check_admin
   
   def index
-    
+    @admins = User.where(:admin => true)
   end
+  
+  def create
+    if admin = User.find(:first, :conditions => [ "name = ?", params[:name]])
+      
+      if admin.admin == false
+        admin.admin = true
+        
+        if admin.save
+          flash[:notice] = "Added new Admin!"
+        end
+        
+      else
+        flash[:alert] = "User is already an Admin!"
+      end   
+      
+    else
+      flash[:alert] = "Username not found!"
+    end
+    
+    redirect_to :back
+  end
+  
 end
