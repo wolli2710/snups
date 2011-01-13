d50b197d14cff8eefa7218471eeae152f1299d11
Franz Torghele <ftorghele.mmt-b2009@fh-salzburg.ac.at>
2011-01-13 17:08:58 +0100

    images controller: moved methods to model

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,10 +11,13 @@ class ImagesController < ApplicationController
     status = "ERROR" 
     if user = User.find_by_name(params[:username])
       if user.valid_password?(params[:password])
-        if mission = Mission.find_by_id(params[:mission_id])
+        if Mission.find_by_id(params[:mission_id])
           if params[:image]
             status = "SUCCESS"
-            handle_upload(user)
+            image = Image.new(:user_id => user.id,
+                              :mission_id => params[:mission_id])
+            Image.handle_upload(image, params)
+            image.save
           else
             status = "NO IMAGE"
           end
@@ -29,37 +32,7 @@ class ImagesController < ApplicationController
     end
     render :json => {:status => status}
   end
-  
-  def handle_upload(user)
-    #database---------------------------------------------------#
-    image = Image.new(:user_id => user.id,
-                      :mission_id => params[:mission_id])
-    image_name = Time.now.to_s+params[:username]
-    image.nameHash = Digest::SHA1.hexdigest(image_name)
-    #-----------------------------------------------------------#
-    
-    #create images----------------------------------------------#
-    input_path = params[:image].path()
-    
-    main_path = Rails.root.to_s + "/public/images/upload/"
-    output_high = main_path + image.nameHash + "_high.jpg"
-    output_medium = main_path + image.nameHash + "_medium.jpg"
-    output_low = main_path + image.nameHash + "_low.jpg"
-    
-    uploaded_image = MiniMagick::Image.open(input_path)
-    
-    uploaded_image.write  output_high
-    
-    uploaded_image = resize_and_crop_banner(uploaded_image, 300, 150)
-    uploaded_image.write output_medium
-    
-    uploaded_image = resize_and_crop(uploaded_image, 100)
-    uploaded_image.write output_low
-    
-    #store to database-----------------------------------------#
-    image.save()
-  end
-   
+
   def destroy
     if image = Image.find(:first, :conditions => [ "id = ?", params[:id]])
       if image.destroy
@@ -70,10 +43,10 @@ class ImagesController < ApplicationController
     end
     redirect_to :back
   end
-  
+
   def edit
     if image = Image.find(:first, :conditions => [ "id = ?", params[:id]])
-      
+
       image.report_count = 0
       if image.save
         for report in Report.where(:image_id => image.id)
@@ -85,33 +58,5 @@ class ImagesController < ApplicationController
       end
     end
     redirect_to :back
-  end
-  
-  protected
-  def resize_and_crop(image, size)
-    if image[:width] < image[:height]
-      shave_off = ((
-        image[:height]-
-        image[:width])/2).round
-        image.shave("0x#{shave_off}")
-    elsif image[:width] > image[:height]
-      shave_off = ((
-        image[:width]-
-        image[:height])/2).round
-        image.shave("#{shave_off}x0")
-    end
-    image.resize size
-    return image
-  end
-  
-  def resize_and_crop_banner(image, w, h)
-    size = w.to_s + "x" + h.to_s
-    
-    image = resize_and_crop(image, w)
-    
-    shave_h = ((image[:height]- h)/2).round
-    image.shave("0x#{shave_h}")
-
-    return image
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -14,11 +14,62 @@ class Image < ActiveRecord::Base
   
   validates :user_id, :presence => true, :numericality => true
   validates :mission_id, :presence => true, :numericality => true
-  validates :nameHash, :presence => true
 
   def delete_image_files
     File.delete("#{Rails.root}/public/images/upload/#{self.nameHash}_high.jpg")
     File.delete("#{Rails.root}/public/images/upload/#{self.nameHash}_medium.jpg")
     File.delete("#{Rails.root}/public/images/upload/#{self.nameHash}_low.jpg")
+  end
+
+  def self.handle_upload(image, params)
+
+    image_name = Time.now.to_s+params[:username]
+    image.nameHash = Digest::SHA1.hexdigest(image_name)
+
+    input_path = params[:image].path()
+
+    main_path = Rails.root.to_s + "/public/images/upload/"
+    output_high = main_path + image.nameHash + "_high.jpg"
+    output_medium = main_path + image.nameHash + "_medium.jpg"
+    output_low = main_path + image.nameHash + "_low.jpg"
+
+    uploaded_image = MiniMagick::Image.open(input_path)
+
+    uploaded_image.write output_high
+
+    uploaded_image = resize_and_crop_banner(uploaded_image, 300, 150)
+    uploaded_image.write output_medium
+
+    uploaded_image = resize_and_crop(uploaded_image, 100)
+    uploaded_image.write output_low
+
+    return image
+  end
+  
+  def self.resize_and_crop(image, size)
+    if image[:width] < image[:height]
+      shave_off = ((
+        image[:height]-
+        image[:width])/2).round
+        image.shave("0x#{shave_off}")
+    elsif image[:width] > image[:height]
+      shave_off = ((
+        image[:width]-
+        image[:height])/2).round
+        image.shave("#{shave_off}x0")
+    end
+    image.resize size
+    return image
+  end
+
+  def self.resize_and_crop_banner(image, w, h)
+    size = w.to_s + "x" + h.to_s
+
+    image = resize_and_crop(image, w)
+
+    shave_h = ((image[:height]- h)/2).round
+    image.shave("0x#{shave_h}")
+
+    return image
   end
 end
